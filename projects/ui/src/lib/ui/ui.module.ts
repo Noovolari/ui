8a58cf5a8d86fb57126c2dd64d9aa267e6b76d20
andreacavagna <andrea.cavagna@noovolari.com>
2023-03-23 11:26:02 +0100

    chore: added default scss for components

--- projects/ui/src/lib/ui/ui.module.ts
+++ projects/ui/src/lib/ui/ui.module.ts
@@ -4,9 +4,10 @@ import { ButtonComponent } from "./component/button/button.component";
 import { InputFieldComponent } from './component/input-field/input-field.component';
 import { NoovolariButtonComponent } from './component/noovolari-button/noovolari-button.component';
 import { NoovolariFilterComponent } from './component/noovolari-filter/noovolari-filter.component';
+import { TestoneComponent } from './component/testone/testone.component';
 
 @NgModule({
-  declarations: [ButtonComponent, InputFieldComponent, NoovolariButtonComponent, NoovolariFilterComponent],
+  declarations: [ButtonComponent, InputFieldComponent, NoovolariButtonComponent, NoovolariFilterComponent, TestoneComponent],
   imports: [
     CommonModule
   ],
