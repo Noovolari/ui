7786e4998155b5ddaf3c7f013899fa4c85d79abb
andreacavagna <andrea.cavagna@noovolari.com>
2023-03-23 11:44:07 +0100

    fix: removed test component in module

--- projects/ui/src/lib/ui/ui.module.ts
+++ projects/ui/src/lib/ui/ui.module.ts
@@ -4,10 +4,9 @@ import { ButtonComponent } from "./component/button/button.component";
 import { InputFieldComponent } from './component/input-field/input-field.component';
 import { NoovolariButtonComponent } from './component/noovolari-button/noovolari-button.component';
 import { NoovolariFilterComponent } from './component/noovolari-filter/noovolari-filter.component';
-import { TestoneComponent } from './component/testone/testone.component';
 
 @NgModule({
-  declarations: [ButtonComponent, InputFieldComponent, NoovolariButtonComponent, NoovolariFilterComponent, TestoneComponent],
+  declarations: [ButtonComponent, InputFieldComponent, NoovolariButtonComponent, NoovolariFilterComponent],
   imports: [
     CommonModule
   ],
