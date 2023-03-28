ccdd77d3fb24f8243dba11ae56f82f5c6b1dcf6c
andreacavagna <andrea.cavagna@noovolari.com>
2023-03-28 11:56:21 +0200

    fix: removed field in module

--- projects/ui/src/lib/ui/ui.module.ts
+++ projects/ui/src/lib/ui/ui.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ButtonComponent } from "./component/button/button.component";
 import { NoovolariButtonComponent } from './component/noovolari-button/noovolari-button.component';
 import { NoovolariFilterComponent } from './component/noovolari-filter/noovolari-filter.component';
-import {InputFieldComponent} from "./component/input-field/input-field.component";
+
 
 @NgModule({
-  declarations: [ButtonComponent, InputFieldComponent, NoovolariButtonComponent, NoovolariFilterComponent],
+  declarations: [ButtonComponent, NoovolariButtonComponent, NoovolariFilterComponent],
   imports: [
     CommonModule
   ],
-  exports: [ButtonComponent, InputFieldComponent, NoovolariButtonComponent, NoovolariFilterComponent]
+  exports: [ButtonComponent, NoovolariButtonComponent, NoovolariFilterComponent]
 })
 export class UiModule { }
