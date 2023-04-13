269e5957620cf6f0d2b44c6bb228baebb26a5043
andreacavagna <andrea.cavagna@noovolari.com>
2023-04-13 17:46:47 +0200

    fix: fixed publish script to publish lib only

--- projects/ui/src/public-api.ts
+++ projects/ui/src/public-api.ts
@@ -2,4 +2,13 @@
  * Public API Surface of ui
  */
 
+/* MODULES */
 export * from "./lib/ui/ui.module";
+
+/* COMPONENTS */
+export {ButtonComponent} from "./lib/ui/component/button/button.component";
+export {NoovolariButtonComponent} from "./lib/ui/component/noovolari-button/noovolari-button.component";
+export {NoovolariChipComponent} from "./lib/ui/component/noovolari-chip/noovolari-chip.component";
+
+/* COMPOSITES */
+export {NoovolariFilterComponent} from "./lib/ui/composite/noovolari-filter/noovolari-filter.component";
