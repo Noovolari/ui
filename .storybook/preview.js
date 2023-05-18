b389ac1eeafa6d5c3a6933631e42c9e9265bfd27
Marco Vanetti <marco.vanetti@gmail.com>
2023-05-18 17:57:58 +0200

    no message

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
 import {addons} from "@storybook/addons";
 import noovolariTheme from "./noovolariTheme";
+import '!style-loader!css-loader!sass-loader!../projects/ui/src/lib/ui/assets/scss/_global.scss';
 setCompodocJson(docJson);
 
 addons.setConfig({
