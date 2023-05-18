827b5a70abfe0068edd7f730c37e1392f94de623
Marco Vanetti <marco.vanetti@gmail.com>
2023-05-18 17:43:00 +0200

    chore: trying to upgrade to angular 13

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,11 @@
 module.exports = {
-  "stories": [
-    "../projects/ui/src/lib/**/*.stories.mdx",
-    "../projects/ui/src/lib/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-scss",
-    "storybook-dark-mode"
-  ],
-  "framework": "@storybook/angular",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  "stories": ["../projects/ui/src/lib/**/*.stories.mdx", "../projects/ui/src/lib/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-scss", "storybook-dark-mode", "@storybook/addon-mdx-gfm"],
+  "framework": {
+    name: "@storybook/angular",
+    options: {}
+  },
+  docs: {
+    autodocs: true
   }
-}
+};
